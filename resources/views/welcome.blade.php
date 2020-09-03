61a178712e4164376a0a6b15df7b54fe15c37cc6
stanliwise <stanliwise@gmail.com>
2020-09-03 01:32:53 +0100

    imported css and js files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+            <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -142,5 +143,6 @@
                 </code>
             </div>
         </div>
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </body>
 </html>
